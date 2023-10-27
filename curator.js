6600d4a9ad59aef2dbfa567db1a94ffd3a93d208
playfluent <joe@viasana.co>
2023-10-27 12:50:01 -0700

    curator.io + IG fix
    attempted to make the loading faster
    - ended up with just a small hack to prevent a big screen jump

---
+++ curator.js
@@ -0,0 +1,10 @@
+ /* curator-feed-default-feed-layout */
+(function(){
+  var i,e,d=document,s="script";
+  i=d.createElement("script");
+  i.async=0;
+  i.charset="UTF-8";
+  i.src="https://cdn.curator.io/published/108f7c79-8ffc-467c-a1bf-37780a6a341e.js";
+  e=d.getElementsByTagName(s)[0];
+  e.parentNode.insertBefore(i, e);
+})();
