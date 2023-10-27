62df6732c47c6a1fad54ec64b015206eee10f813
playfluent <joe@viasana.co>
2023-10-27 12:57:41 -0700

    Update curator.js

--- curator.js
+++ curator.js
@@ -2,7 +2,7 @@
 (function(){
   var i,e,d=document,s="script";
   i=d.createElement("script");
-  i.async=0;
+  i.async=1;
   i.charset="UTF-8";
   i.src="https://cdn.curator.io/published/108f7c79-8ffc-467c-a1bf-37780a6a341e.js";
   e=d.getElementsByTagName(s)[0];
