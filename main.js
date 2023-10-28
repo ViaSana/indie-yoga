1fb918fc9cbd171c0a2c7493883dee9ae479eabe
playfluent <joe@viasana.co>
2023-10-27 17:06:00 -0700

    Final Launch Candidate
    - color conversion complete
    - menu nav updated
    - workshop loop set to cycle

--- main.js
+++ main.js
@@ -25,5 +25,4 @@ $(".testimonial-slider-container").slick({
 // Hamburger Menu
 $('.hamburger-button').click(function(){
     $('.mobile-menu').fadeToggle(100);
-    $(this).toggleClass('active');
 });
